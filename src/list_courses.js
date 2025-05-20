f34bcb56adcbee8075c09573e0e642cbd18f87b5
KeniaKodigo <kenia.paz@kodigo.org>
2025-05-19 20:50:28 -0600

    update list courses

--- src/list_courses.js
+++ src/list_courses.js
@@ -1,20 +1,27 @@
 import courses from './data/courses.json'
 
 export function getCourses(){
+    console.log(courses)
     const courseList = document.querySelector('.container-courses');
 
+    //arreglo
     courses.map(course => {
+        //etiqueta html
         const article = document.createElement('article');
+        /** agregar clases, eliminar, verificar */
         article.classList.add('col-md-4');
+
+        //desestructuracion de datos
+        const { title, image, description, price, id } = course
 
         article.innerHTML = `
             <div class="card">
-                <img src=${course.image} class="card-img-top" alt="clase 1">
+                <img src=${image} class="card-img-top" alt="clase 1">
                 <div class="card-body">
-                    <h5 class="card-title">${course.title}</h5>
-                    <p class="card-text">${course.description}}</p>
-                    <p class="price fw-bold">${course.price}</p>
-                    <button class="btn btn-primary w-100 button-cart" data-id=${course.id}><i class="bi bi-bag-plus-fill"></i> Agregar al carrito</button>
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}}</p>
+                    <p class="price fw-bold">${price}</p>
+                    <button class="btn btn-primary w-100 button-cart" data-id=${id}><i class="bi bi-bag-plus-fill"></i> Agregar al carrito</button>
                 </div>
             </div>
         `
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import { getCourses } from './list_courses';
 
+//estar atento cuando se cargue la pagina
 document.addEventListener('DOMContentLoaded',() => {
+    //mostrar la lista de cursos
     getCourses();
 }) 
